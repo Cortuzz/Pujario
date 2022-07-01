fc1b7a3175868a7ad14df34184c49fd13f59dcb5
Oleg <52497929+Cortuzz@users.noreply.github.com>
2022-07-01 21:18:23 +0700

    Revert "Revert "refactor/math""

---
+++ MathLibrary/MathLibrary.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+
+extern "C" __declspec(dllexport) double OctavePerlin(double x, double y, double z, int octaves, double persistence);
+
+extern "C" __declspec(dllexport)double Perlin(double x, double y, double z);
+
+extern "C" __declspec(dllexport)double Lerp(double start, double end, double ratio);
+
+extern "C" __declspec(dllexport)double Fade(double t);
+
+extern "C" __declspec(dllexport)double Gradient(int hash, double x, double y, double z);
